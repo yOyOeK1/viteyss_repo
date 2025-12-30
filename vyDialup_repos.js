c9bd259340c1eb8294a7826ad1f651232506d089
Bartlomiej Ceglik <yoyoek@wp.pl>
2025-12-30 06:38:15 -0500

    repo 0.0.1

--- vyDialup_repos.js
+++ vyDialup_repos.js
@@ -1,1 +1,26 @@
-{ "apps": ["blank_repo_test"] }
+{
+  "apps": [
+    {
+      "name": "blank_repo_test",
+      "type": "dir"
+    },
+    {
+      "name": "settings1",
+      "type": "site",
+      "url": "/home/yoyo/Apps/viteyss-site-settings1"
+    },
+    {
+      "name": "2qest",
+      "type": "site",
+      "url": "/home/yoyo/Apps/viteyss-site-2qest",
+      "deps": {
+        "settings1": "0.0.0"
+      }
+    },
+    {
+      "name": "hello-world",
+      "type": "site",
+      "url": "https://github.com/yOyOeK1/viteyss-site-hello-world.git"
+    }
+  ]
+}
