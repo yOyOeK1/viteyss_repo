879fd9dbcfb4b59d54ac47de96d081bd5eb2dac3
Bartlomiej Ceglik <yoyoek@wp.pl>
2026-01-01 15:48:31 -0500

    adding more no repo

--- vyDialup/vyDialup_repos.js
+++ vyDialup/vyDialup_repos.js
@@ -10,6 +10,20 @@
       "url": "https://github.com/yOyOeK1/oiyshTerminal",
       "subdir": "ySS_calibration/sites/blank"
     },
+   
+    {
+      "name": "iloo_nav_3d",
+      "type": "dirFromGit",
+      "url": "https://github.com/yOyOeK1/oiyshTerminal",
+      "subdir": "ySS_calibration/sites/ilo_nav_3d"
+    },
+    {
+      "name": "test_Three.js",
+      "type": "dirFromGit",
+      "url": "https://github.com/yOyOeK1/oiyshTerminal",
+      "subdir": "ySS_calibration/sites/test_Three.js"
+    },
+
     {
       "name": "settings1",
       "type": "site",
